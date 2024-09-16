9953fecb8047bd0b2e1315e4325dcfa15a8f4ca4
Henrique Yule <yzhenrique@gmail.com>
2024-09-16 07:37:41 -0400

    Adicionando rota de metas cumpridas

--- src/http/server.ts
+++ src/http/server.ts
@@ -3,6 +3,7 @@ import { serializerCompiler, validatorCompiler, type ZodTypeProvider } from "fas
 import { createGoal } from '../functions/create-goal';
 import z from 'zod';
 import { getWeekPendingGoals } from '../functions/get-week-pending-goals';
+import { CreateGoalCompletion } from '../functions/create-goal-completion';
 
 const app = fastify().withTypeProvider<ZodTypeProvider>()
 
@@ -28,6 +29,19 @@ app.post('/goals', {
         title,
         desiredWeeklyFrequency
     })
+})
+
+app.post('/completions', {
+	schema: {
+			body: z.object({
+				goalId: z.string()
+			}),
+	}
+}, async request => {
+	const { goalId } = request.body
+	await CreateGoalCompletion({
+		goalId
+	})
 })
 
 app.listen({
