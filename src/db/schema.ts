6254a4060f7c679556c47a1a94a757c967789dd4
Henrique Yule <yzhenrique@gmail.com>
2024-10-02 20:52:05 -0400

    Ajustando data atual na criação de dados

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -5,11 +5,11 @@ export const goals = pgTable('goals', {
     id: text('id').primaryKey().$defaultFn(() => createId()),
     title: text('title').notNull(),
     desiredWeeklyFrequency: integer('desired_weekly_frequency').notNull(),
-    createdAt: timestamp('created_at', { withTimezone: true }).notNull().defaultNow(),
+    createdAt: timestamp('created_at', { withTimezone: true }).notNull().defaultNow().$defaultFn(() => new Date(Date.now() - 24 * 60 * 60 * 1000)),
 })
 
 export const goalCompletions = pgTable('goal_completions', {
     id: text('id').primaryKey().$defaultFn(() => createId()),
     goalId: text('goal_id').notNull().references(() => goals.id),
-    createdAt: timestamp('created_at', { withTimezone: true }).notNull().defaultNow(),
+    createdAt: timestamp('created_at', { withTimezone: true }).notNull().defaultNow().$defaultFn(() => new Date(Date.now() - 24 * 60 * 60 * 1000)),
 })
